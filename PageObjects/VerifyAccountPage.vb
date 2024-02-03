1fb5fe6c0666c2833c29d7c7a572d7076008f196
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-02-03 21:20:20 +0700

    Add test suit "VerifyAccount"

---
+++ PageObjects/VerifyAccountPage.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Text.RegularExpressions
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports OpenQA.Selenium
+
+Public Class VerifyAccountPage
+    Private ReadOnly driver As IWebDriver
+    Private ReadOnly pageElements As New Dictionary(Of String, String) From {
+        {"confirmationCodeInfo", "//small[@class='info']"},
+        {"confirmationInputs", "(//input[@type='number'])[{index}]"},
+        {"successText", ".info.success"}
+    }
+
+    Public Sub New(driver As IWebDriver)
+        Me.driver = driver
+    End Sub
+
+    Public Function GetConfirmationCode() As String
+        Dim confirmationCodeElement As IWebElement = driver.FindElement(By.XPath(pageElements("confirmationCodeInfo")))
+        Dim confirmationCode As String = Regex.Replace(confirmationCodeElement.Text, "[^\d]", "")
+        Return confirmationCode
+    End Function
+
+    Public Sub InputConfirmationCode(code As String)
+        Dim codes As String() = code.ToCharArray().Select(Function(c) c.ToString()).ToArray()
+        Dim codeLength As Integer = codes.Length
+        Dim confirmationInputTemplate As String = pageElements("confirmationInputs")
+
+        For i As Integer = 0 To codeLength - 1
+            Dim confirmationInput = confirmationInputTemplate.Replace("{index}", (i + 1).ToString())
+            driver.FindElement(By.XPath(confirmationInput)).SendKeys(codes(i))
+        Next
+    End Sub
+
+    Public Sub VerifySuccessTextVisible()
+        Dim isSuccessTextVisible As Boolean = driver.FindElements(By.CssSelector(pageElements("successText"))).Count <> 0
+        Assert.IsTrue(isSuccessTextVisible, "successText is not visible.")
+    End Sub
+
+    Public Sub VerifySuccessTextNotVisible()
+        Dim isSuccessTextVisible As Boolean = driver.FindElements(By.CssSelector(pageElements("successText"))).Count <> 0
+        Assert.IsFalse(isSuccessTextVisible, "successText is still visible.")
+    End Sub
+
+End Class
---
+++ TestCases/VerifyAccount.vb
@@ -0,0 +1,40 @@
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Namespace QaPlayGround
+    <TestClass>
+    Public Class VerifyAccount
+        Private ReadOnly common As Common
+        Private ReadOnly verifyAccountPage As VerifyAccountPage
+        ReadOnly browserName As String = If(Environment.GetEnvironmentVariable("BROSWER"), "Chrome")
+
+        Public Sub New()
+            common = New Common(browserName)
+            verifyAccountPage = New VerifyAccountPage(common.GetDriver())
+        End Sub
+
+        <TestInitialize>
+        Sub TestInitialize()
+            common.GoToUrl("https://qaplayground.dev/apps/verify-account/")
+        End Sub
+
+        <TestCleanup>
+        Sub TestCleanup()
+            common.Dispose()
+        End Sub
+
+        <TestMethod>
+        Sub VerifyAccount_Success()
+            Dim code = verifyAccountPage.GetConfirmationCode()
+            verifyAccountPage.InputConfirmationCode(code)
+            verifyAccountPage.VerifySuccessTextVisible()
+        End Sub
+
+
+        <TestMethod>
+        Sub VerifyAccount_NotSuccess()
+            verifyAccountPage.InputConfirmationCode("123456")
+            verifyAccountPage.VerifySuccessTextNotVisible()
+        End Sub
+
+    End Class
+End Namespace
