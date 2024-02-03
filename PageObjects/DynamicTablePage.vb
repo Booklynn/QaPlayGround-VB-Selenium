7dc33743722d72066a13b87fa1ecece6c9d4877b
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-02-03 17:54:26 +0700

    Use data-driven in test cases

--- PageObjects/DynamicTablePage.vb
+++ PageObjects/DynamicTablePage.vb
@@ -4,27 +4,56 @@ Imports OpenQA.Selenium
 Public Class DynamicTablePage
     Private ReadOnly driver As IWebDriver
     Private ReadOnly pageElements As New Dictionary(Of String, String) From {
-        {"spiderManImage", "img[src='./img/spiderman.jpg']"},
-        {"spiderManEmail", "//div[text()='spider-man@avengers.com']"},
-        {"spiderManName", "//span[text()='Peter Parker']"}
+        {"columnSuperHero", "//th[normalize-space(text())='Superhero']"},
+        {"columnStatus", "//th[normalize-space(text())='Status']"},
+        {"columnRealName", "//th[normalize-space(text())='Real Name']"},
+        {"superHeroName", "//div[text()='{superHeroName}']"},
+        {"superHeroImage", "img[src='./img/{superHeroImage}']"},
+        {"superHeroEmail", "//div[text()='{superHeroEmail}']"},
+        {"superHeroRealName", "//span[text()='{superHeroRealName}']"}
     }
 
     Public Sub New(driver As IWebDriver)
         Me.driver = driver
     End Sub
 
-    Public Sub VerifySpiderManImageVisible()
-        Dim isSpiderManImageVisible As Boolean = driver.FindElements(By.CssSelector(pageElements("spiderManImage"))).Count > 0
-        Assert.IsTrue(isSpiderManImageVisible, "Spider-Man image is not visible.")
+    Public Sub VerifySuperHeroNameVisible(name As String)
+        Dim superHeroName = pageElements("superHeroName").Replace("{superHeroName}", name)
+        Dim isSuperHeroNameVisible As Boolean = driver.FindElements(By.XPath(superHeroName)).Count <> 0
+        Assert.IsTrue(isSuperHeroNameVisible, $"{superHeroName} is not visible.")
     End Sub
 
-    Public Sub VerifySpiderManEmailVisible()
-        Dim isSpiderManEmailVisible As Boolean = driver.FindElements(By.XPath(pageElements("spiderManEmail"))).Count > 0
-        Assert.IsTrue(isSpiderManEmailVisible, "Spider-Man email is not visible.")
+    Public Sub VerifySuperHeroImageVisible(imageName As String)
+        Dim superHeroImageName = pageElements("superHeroImage").Replace("{superHeroImage}", imageName)
+        Dim isSuperHeroImageVisible As Boolean = driver.FindElements(By.CssSelector(superHeroImageName)).Count <> 0
+        Assert.IsTrue(isSuperHeroImageVisible, $"{superHeroImageName} is not visible.")
     End Sub
 
-    Public Sub VerifySpiderManNameVisible()
-        Dim isSpiderManNameVisible As Boolean = driver.FindElements(By.XPath(pageElements("spiderManName"))).Count > 0
-        Assert.IsTrue(isSpiderManNameVisible, "Spider-Man name is not visible.")
+    Public Sub VerifySuperHeroEmailVisible(email As String)
+        Dim superHeroEmail = pageElements("superHeroEmail").Replace("{superHeroEmail}", email)
+        Dim isSuperHeroEmailVisible As Boolean = driver.FindElements(By.XPath(superHeroEmail)).Count <> 0
+        Assert.IsTrue(isSuperHeroEmailVisible, $"{superHeroEmail} is not visible.")
     End Sub
+
+    Public Sub VerifySuperHeroRealNameVisible(realName As String)
+        Dim superHeroRealName = pageElements("superHeroRealName").Replace("{superHeroRealName}", realName)
+        Dim isSuperHeroRealNameVisible As Boolean = driver.FindElements(By.XPath(superHeroRealName)).Count <> 0
+        Assert.IsTrue(isSuperHeroRealNameVisible, $"{superHeroRealName} is not visible.")
+    End Sub
+
+    Public Sub VerifySuperColumnVisible()
+        Dim isSuperColumnVisible As Boolean = driver.FindElements(By.XPath(pageElements("columnSuperHero"))).Count <> 0
+        Assert.IsTrue(isSuperColumnVisible, "columnSuperHero is not visible.")
+    End Sub
+
+    Public Sub VerifyStatusColumnVisible()
+        Dim isStatusColumnVisible As Boolean = driver.FindElements(By.XPath(pageElements("columnStatus"))).Count <> 0
+        Assert.IsTrue(isStatusColumnVisible, "columnStatus is not visible.")
+    End Sub
+
+    Public Sub VerifyRealNameColumnVisible()
+        Dim isRealNameColumnVisible As Boolean = driver.FindElements(By.XPath(pageElements("columnRealName"))).Count <> 0
+        Assert.IsTrue(isRealNameColumnVisible, "columnRealName is not visible.")
+    End Sub
+
 End Class
--- TestCases/DynamicTable.vb
+++ TestCases/DynamicTable.vb
@@ -22,12 +22,24 @@ Namespace QaPlayGround
         End Sub
 
         <TestMethod>
-        Sub ShouldDisplaySpiderMan()
-            dynamicTablePage.VerifySpiderManImageVisible()
-            dynamicTablePage.VerifySpiderManEmailVisible()
-            dynamicTablePage.VerifySpiderManNameVisible()
+        <DataRow("Spider-Man", "spiderman.jpg", "spider-man@avengers.com", "Peter Parker")>
+        <DataRow("Doctor Strange", "doctor-strange.jpg", "doctor-strange@avengers.com", "Stephen Vincent Strange")>
+        <DataRow("Black Widow", "black-widow.jpg", "black-widow@avengers.com", "Natasha Alianovna Romanova")>
+        <DataRow("Deadpool", "deadpool.jpg", "deadpool@avengers.com", "Wade Wilson")>
+        <DataRow("Captain America", "captain-america.jpg", "captain-america@avengers.com", "Steve Rogers")>
+        Sub VerifySuperHeroesInformation_DisplayedCorrectly(name As String, imageName As String, email As String, realName As String)
+            dynamicTablePage.VerifySuperHeroNameVisible(name)
+            dynamicTablePage.VerifySuperHeroImageVisible(imageName)
+            dynamicTablePage.VerifySuperHeroEmailVisible(email)
+            dynamicTablePage.VerifySuperHeroRealNameVisible(realName)
+        End Sub
+
+        <TestMethod>
+        Sub VerifyAllColumns_DisplayedCorrectly()
+            dynamicTablePage.VerifySuperColumnVisible()
+            dynamicTablePage.VerifyStatusColumnVisible()
+            dynamicTablePage.VerifyRealNameColumnVisible()
         End Sub
 
     End Class
 End Namespace
-
